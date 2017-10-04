1e223ecd874216ee507a6c6162a47392d4fbace2
Harrison Lingren <harrisonlingren@gmail.com>
2017-10-04 17:10:16 +0100

    CORS fix

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ let bodyParser = require('body-parser');
 require('dotenv').config()
 
 let app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
 
 // connect to db here, handle successful and failed conns
 // let mongoose = require('mongoose');
@@ -13,17 +11,22 @@ app.use(bodyParser.urlencoded({extended: true}));
 // let db = mongoose.connection
 
 // let bookingModel = require('models.js')(mongoose);
+
+let allowCrossDomain = function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    next();
+};
 let appRoutes = require('./routes.js');
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
-
+app.use(allowCrossDomain);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api', appRoutes);
 
 let port = process.env.PORT || 8080
 let server = app.listen(port, function() {
     console.log('Listening on port %s...', server.address().port);
 });
+
