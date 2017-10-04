bc9dc2ecfe3a9ed684c1b82339b4677fb3528f48
Harrison Lingren <harrisonlingren@gmail.com>
2017-10-04 22:09:13 +0100

    add create method, change responses to json

--- routes.js
+++ routes.js
@@ -2,14 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 // default
-router.get('/', function(req, res) {
+router.get('/', (req, res) => {
     // welcome message and 204 status
     console.log('got connection!')
     res.status(200).send('Welcome to the assignment 1 API!');
 });
 
 // get booking
-router.get('/booking/:id', function(req, res) {
+router.get('/booking/:id', (req, res) => {
     // search ID from req params
     let found = false;
     let searchId = req.params.id;
@@ -20,16 +20,42 @@ router.get('/booking/:id', function(req, res) {
     // return 200 and send json if present. 
     // otherwise, send 404 and 'not found'
     if (found) {
-        res.status(200).json(foundObj);
+        let resData = {
+            'message': 'Found record!',
+            'data': foundObj
+        }
+        res.status(200).json(resData);
     } else {
-        res.status(404).send('Error: booking ' + searchId + ' was not found')
+        let resData = {
+            'message': 'Error: booking ' + searchId + ' was not found',
+            'data': null
+        }
+        res.status(404).json(resData);
     }
+});
+
+router.post('/create', (req, res) => {
+    console.log('create req', req.body);
+    let recvObj = {
+        'first': req.body.first,
+        'last': req.body.last,
+        'email': req.body.email
+    };
+    // TODO: insert new booking, return created ID
+    let newObj = createNewBooking(recvObj);
+    resData = {
+        'message': 'Record created!',
+        'data': newObj
+    }
+    res.status(201).json(resData);
 });
 
 // search db for a booking by ID
 function findBookingById(id) {
+    // TODO: add db search here
     if (id == '1') {
         return {
+            'id': 1,
             'first': 'Joe',
             'last': 'Schmoe',
             'email': 'lameboi45@gmail.com'
@@ -37,6 +63,19 @@ function findBookingById(id) {
     } else {
         return null;
     }
+}
+
+// insert new booking and return created object
+function createNewBooking(bookingObj) {
+    // TODO: add db stuff here
+    let newId = 0;
+    let newObj = {
+        'id': newId,
+        'first': bookingObj.first,
+        'last': bookingObj.last,
+        'email': bookingObj.email
+    }
+    return newObj
 }
 
 module.exports = router;
