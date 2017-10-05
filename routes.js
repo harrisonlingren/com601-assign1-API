ca1f1df38e4bbd0395dab4f1423c8e208a8b97ec
Harrison Lingren <harrisonlingren@gmail.com>
2017-10-05 12:55:18 +0100

    db integration

--- routes.js
+++ routes.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+var MongoClient = require('mongodb').MongoClient,
+    assert = require('assert');
+
+var db_conn_str = process.env.DB_CONN_STR || null;
+if (!db_conn_str) {
+    console.error('DB_CONN_STR variable was not set!');
+}
+
 // default
 router.get('/', (req, res) => {
     // welcome message and 204 status
@@ -8,74 +16,79 @@ router.get('/', (req, res) => {
     res.status(200).send('Welcome to the assignment 1 API!');
 });
 
-// get booking
+// READ method
 router.get('/booking/:id', (req, res) => {
     // search ID from req params
-    let found = false;
-    let searchId = req.params.id;
+    id = parseInt(req.params.id);
+    console.log('searchId: ', id);
 
-    let foundObj = findBookingById(searchId);
-    found = !!foundObj;
+    // connect to db
+    MongoClient.connect(db_conn_str, (err, db) => {
+        assert.equal(null, err);
+        console.log('searching for id: %s', id);
 
-    // return 200 and send json if present. 
-    // otherwise, send 404 and 'not found'
-    if (found) {
-        let resData = {
-            'message': 'Found record!',
-            'data': foundObj
-        }
-        res.status(200).json(resData);
-    } else {
-        let resData = {
-            'message': 'Error: booking ' + searchId + ' was not found',
-            'data': null
-        }
-        res.status(404).json(resData);
-    }
+        db.collection('bookings').findOne({ book_id: id }, (err, result) => {
+            assert.equal(null, err);
+
+            if (!result) {
+                let resData = {
+                    'message': 'Error: booking ' + id + ' not found',
+                    'data': null
+                }; res.status(404).json(resData);
+
+            } else {
+                console.log('result found: ', result);
+                assert.equal(id, result.book_id);
+                
+                let resData = {
+                    'message': 'Record found!',
+                    'data': result
+                }; res.status(200).json(resData);
+            }
+
+            db.close();
+        });
+    });
 });
 
+// CREATE method
 router.post('/create', (req, res) => {
     console.log('create req', req.body);
     let recvObj = {
+        'book_id': 0,
         'first': req.body.first,
         'last': req.body.last,
         'email': req.body.email
     };
-    // TODO: insert new booking, return created ID
-    let newObj = createNewBooking(recvObj);
-    resData = {
-        'message': 'Record created!',
-        'data': newObj
-    }
-    res.status(201).json(resData);
+    // creates a new booking and returns the object data
+    // connect to db
+    MongoClient.connect(db_conn_str, (err, db) => {
+        assert.equal(null, err);
+        console.log('Connected to db successfully');
+
+        // increment book_id
+        let newId = 0;
+        db.collection('bookings').count((err, count) => {
+            assert.equal(null, err);
+            console.log("count: ", count);
+            newId = count;
+            recvObj.book_id = newId;
+
+            // insert into db
+            db.collection('bookings').insertOne(recvObj, (err, r) => {
+                assert.equal(null, err);
+                assert.equal(1, r.insertedCount);
+
+                // send response
+                resData = {
+                    'message': 'Record created!',
+                    'data': recvObj
+                }; res.status(201).json(resData);
+
+                db.close();
+            });
+        });
+    });
 });
-
-// search db for a booking by ID
-function findBookingById(id) {
-    // TODO: add db search here
-    if (id == '1') {
-        return {
-            'id': 1,
-            'first': 'Joe',
-            'last': 'Schmoe',
-            'email': 'lameboi45@gmail.com'
-        }
-    } else {
-        return null;
-    }
-}
-
-// insert new booking and return created object
-function createNewBooking(bookingObj) {
-    // TODO: add db stuff here
-    let newId = 0;
-    let newObj = {
-        'id': newId,
-        'first': bookingObj.first,
-        'last': bookingObj.last,
-        'email': bookingObj.email
-    }
-    return newObj
-}
 
 module.exports = router;
